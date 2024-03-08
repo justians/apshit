7c09bf4f79b1bf8a5300c3860d233966f0de690d
justians <117990728+justians@users.noreply.github.com>
2024-03-07 20:36:26 -0500

    Update password2.py

--- password2.py
+++ password2.py
@@ -12,16 +12,24 @@ all_characters = {"Lowercase Letters": lowercase_letters, "Uppercase Letters": u
 
 def generate(password_length: int):
     questions = [
-    inquirer.Checkbox('interests',
+    inquirer.Checkbox('selection',
                         message="Make your selection of what types of characters youd like in your password: ",
-                        choices=["Lowercase Letters", "Uppercase Letters", "Uppercase Letter", "Symbols"]
+                        choices=["Lowercase Letters", "Uppercase Letters", "Numbers", "Symbols"]
                         ),
     ]
     answers = inquirer.prompt(questions)
+    #(answers["interests"])
+    user_selection = []
     char_list = []
-    for i in range(length):
-        char_list.append(random.choice(all_characters))
-    print("".join(char_list))
+    for i in answers["selection"]:
+        for x in all_characters:
+            if i == x:
+                user_selection.extend(all_characters[x])
+                #print(user_selection)
 
+    for u in range(password_length):
+        char_list.append(random.choice(user_selection))
+    final_password = "".join(char_list)
+    print(final_password)
 length = int(input("Input the desired length of your password: "))
 generate(length)
